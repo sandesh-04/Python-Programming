f9f102a8e0d0d6adbea79d8a142de97101407d58
Sandesh Singh <sandesh04singh@gmail.com>
2026-06-21 11:28:04 +0530

    Logical Operator in Python

---
+++ LogicalOperator/AND.py
@@ -0,0 +1,8 @@
+# if applicant have high income and also if have good credit,
+# then eligible for loan
+
+has_high_income = True
+has_good_credit = True
+
+if has_good_credit and has_high_income:
+    print("Eligible for loan.")
---
+++ LogicalOperator/OR.py
@@ -0,0 +1,13 @@
+# if applicant have high income or have good credit,
+# then eligible for loan
+
+has_high_income = True
+has_good_credit = False
+
+if has_good_credit or has_high_income:
+    print("Eligible for loan.")
+
+
+# Logical AND => both condition should be true
+# Logical OR => atleast one condition should be true
+# NOT => inverse the boolean value
