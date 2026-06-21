81710ba1f411a28d64e65ab1383f8aaacbbd72d5
Sandesh Singh <sandesh04singh@gmail.com>
2026-06-21 10:23:34 +0530

    Arithmatic op and their precedence

---
+++ Basics/ArithmaticOp.py
@@ -0,0 +1,30 @@
+print(3 + 5)
+print(5 - 3)
+print(5 * 3)
+print(5 / 3)
+print(5 // 3) # gives the integer value of division
+print( 5 % 3)
+
+# something to the power of something
+print(5 ** 3)
+
+
+# Augumented or Enhanced Assignment operator(+=, -=)
+
+x = 10
+# x = x + 3
+x += 3
+print(x)
+
+
+
+# Operator precedence:
+
+x = (10 + 3) * 5 ** 2
+print(x)
+
+# order of precedence
+# parenthesis()
+# exponentiation **
+# multiplication or division (*, /)
+# add or sub (+, -)
