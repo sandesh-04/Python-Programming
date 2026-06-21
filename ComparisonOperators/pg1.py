0aaaedef6278502f1e0f4e9883aabbc78142b24b
Sandesh Singh <sandesh04singh@gmail.com>
2026-06-21 11:38:16 +0530

    Comaprison operator in python

---
+++ ComparisonOperators/pg1.py
@@ -0,0 +1,10 @@
+# >, < , ==, !=, >=, <=
+
+temperature = 10
+
+if temperature > 35:
+    print("It' a hot day")
+elif temperature <= 34:
+    print("It's not a hot day, it's very good")
+else:
+    print("It's not a hot day")
---
+++ ComparisonOperators/pg2.py
@@ -0,0 +1,8 @@
+name = "John"
+
+if len(name) < 3:
+    print("Name is very short")
+elif len(name) > 10:
+    print("Name is too long")
+else:
+    print("Name looks good")
